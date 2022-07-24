7aace325ea00d6a00c82888733da8ea0e3572b71
IntelliCoach <102197042+IntelliCoach@users.noreply.github.com>
2022-07-24 16:23:02 -0400

    Create video_list.js

---
+++ video_list.js
@@ -0,0 +1,34 @@
+let videos = [{
+    title: "Create a Team",
+    video: "1HVyl5r100iWGeMEWJS1vM3HmaY3OBZMY"
+}, {
+    title: "Add a Player",
+    video: "1Q94L7gOgqVW6FKccmjlcCzw4Hwmjzhpy"
+}, {
+    title: "Remove a Player",
+    video: "14KakYKG4ksclo4g2ukxCoOsyNePiqw92"
+}, {
+    title: "Rank Players",
+    video: "1ROGXOdzlWnzRuOs-CjaJ0lMmg-j299cJ"
+}, {
+    title: "Set Game Specifications",
+    video: "16_8wzMobA9BlMUgCbSMXE29u1O8vBGBj"
+}, {
+    title: "Start a Game",
+    video: "1UC3mbTytbDhaEK__V4PSVEmo-LqHZUDk"
+}, {
+    title: "Use the Game Screen",
+    video: "1jDd0hNS-F_ua1_fi2aAc2f8xaifUffCM"
+}, {
+    title: "Add a Player Who Arrives Late",
+    video: "1nZIuSW5-geYVsnIa_qj2c62LIkWvV5s_"
+}, {
+    title: "Remove an Injured Player",
+    video: "1RFHv1rhphTxfFRPHdTWJWDlToDFz9bVL"
+}, {
+    title: "Edit Formation Midgame",
+    video: "1bjlCqx8tDfSRAVZBf4S2LsxMMt3nZVOt"
+}, {
+    title: "Edit Goalies Midgame",
+    video: "10d3jPWzsZCYnjeEZ_Y8H0XKWkf1W57c1"
+}];
