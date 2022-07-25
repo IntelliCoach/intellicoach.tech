f8b18cc9c69420a52470ac2f211a137ae6bb2ebe
ldmoore <ldmoore0205@gmail.com>
2022-07-24 21:04:17 -0400

    Update support.js

--- support.js
+++ support.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(queryString);
 const videoID = urlParams.get("v");
 if (videoID) {
     document.getElementById("title").innerText = `How Do I ${videos.find(v => v.video === videoID).title}?`
-    document.getElementById("supportVideo").src = `https://drive.google.com/file/d/${videoID}/preview`;
+    document.getElementById("supportVideo").src = `https://drive.google.com/uc?export=download&id=${videoID}`;
 } else {
     window.location = "/";
 }
