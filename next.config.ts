390ccdbab22dec420575a23723502278bbbb77db
ldmoore <lucasdmo@unc.edu>
2025-08-09 14:57:52 -0400

    Revert failed redirect approach

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,8 @@
-import { ANDROID_LINK, IOS_LINK } from "@/lib/links";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  async redirects() {
-    return [
-      {
-        source: "/ios",
-        destination: IOS_LINK,
-        permanent: true,
-      },
-      {
-        source: "/apple",
-        destination: IOS_LINK,
-        permanent: true,
-      },
-      {
-        source: "/android",
-        destination: ANDROID_LINK,
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
