175921c9ea549ca27e518e183eee7e098d708dcb
ldmoore <lucasdmo@unc.edu>
2025-08-09 14:33:29 -0400

    Add /ios, /apple, /android redirects

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,28 @@
+import { ANDROID_LINK, IOS_LINK } from "@/lib/links";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: "/ios",
+        destination: IOS_LINK,
+        permanent: true,
+      },
+      {
+        source: "/apple",
+        destination: IOS_LINK,
+        permanent: true,
+      },
+      {
+        source: "/android",
+        destination: ANDROID_LINK,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
